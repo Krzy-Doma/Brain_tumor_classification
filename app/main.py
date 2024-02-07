74647db10c587e479cd3b63c72eac7278a1b9fe7
Krzysztof Domarecki <107145574+Krzy-Doma@users.noreply.github.com>
2024-02-07 14:45:02 +0100

    small changes

--- app/main.py
+++ app/main.py
@@ -1,20 +1,26 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-from io import BytesIO
 import tensorflow as tf
 
 
-class_names = ['glioma', 'meningioma', 'no tumor', 'pituitary']
+class_names = ['Glioma', 'Meningioma', 'No tumor', 'Pituitary Adenoma']
 
 input_shape = (256, 256, 3)
-    
+
+page_icon = Image.open('../img/pi.jfif')
+
+st.set_page_config(page_title='Brain tumor classfication', page_icon=page_icon)
+
 #Title
 st.title("Brain Tumor classification")
 
 #Description
-st.write("Welcome to the brain tumor classification tool powered by Convolutional Neural Networks! This website allows you to upload an X-ray image of a brain, which is then analyzed by an advanced CNN model to predict one of four possible classes: 'glioma', 'meningioma', 'no tumor', or 'pituitary'. The model achieved 95% accuracy on the test set. Simply upload your X-ray image and let the CNN model offer insights into your brain's health status.")
+st.write(f"Welcome to the brain tumor classification tool powered by Convolutional Neural Networks! This website allows you to upload an MRI image of a brain, which is then analyzed by an advanced CNN model to predict one of four possible classes: **'Glioma', 'Meningioma', 'No tumor', or 'Pituitary Adenoma'**. The model achieved 95% accuracy on the test set. Simply upload your MRI image and let the CNN model offer insights into your brain's health status.")
 
+st.write(f"**Glioma:** A type of brain tumor that starts in the glial cells, which support nerve cells in the brain. They can be either cancerous or non-cancerous.")
+st.write(f"**Meningioma:** A usually non-cancerous brain tumor that forms in the meninges, the protective layers surrounding the brain and spinal cord. It tends to grow slowly over time.")
+st.write(f"**Pituitary Adenoma:** A benign tumor that develops in the pituitary gland, located at the base of the brain. It can disrupt hormone production and cause various symptoms depending on the affected hormones.")
 
 st.subheader('Sample Images used to train model:', divider='grey')
 
@@ -39,7 +45,7 @@ c3.image(resized_images[2], caption=f"{class_names[2]}", use_column_width=True)
 c4.image(resized_images[3], caption=f"{class_names[3]}", use_column_width=True)
 
 #Header
-st.header('Upload your image')
+st.header('Upload MRI image')
 
 
 #Model
@@ -53,7 +59,6 @@ with st.spinner("Loading Model...."):
     
     
 #Upload file
-st.write("Allowed extensions: png, jpeg, jpg")
 file = st.file_uploader("", type=['png', 'jpeg', 'jpg'], label_visibility='hidden')
     
     
@@ -80,6 +85,8 @@ def preprocess_image(uploaded_file):
 
 predicted_class = None
 confidence = None
+
+c1, c2, c3 = st.columns(3)
     
 #Display image
 if file is not None:
@@ -87,7 +94,7 @@ if file is not None:
     img = np.expand_dims(img, axis=0)
     
     #Show image
-    st.image(file, caption='Uploaded Image', use_column_width=True)
+    c2.image(file, caption='Uploaded Image', use_column_width=True)
      
     prediction = model.predict(img)
     
@@ -95,6 +102,7 @@ if file is not None:
     confidence = np.max(prediction[0])
     
 #Display prediction    
+st.write("Press the button below to classify")
 
 button = st.button("Classify", type="primary")
 if predicted_class and confidence and button:
@@ -105,4 +113,8 @@ else:
     st.subheader(f'Confidence     : ---------')
     
 
-    
+st.divider()
+
+st.write("")    
+github = 'https://github.com/Krzy-Doma/brain_tumor_classification/tree/main'
+st.write(f"Check out my [Github](%s)" % github)
